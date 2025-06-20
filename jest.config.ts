db2a1dbe2e9a613d3ca0172351d1a546ff8e31b0
taichi-web-engineer <aogaku415@gmail.com>
2025-06-20 09:46:17 +0900

    UT作成、Issue対応コマンド作成

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,3 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
