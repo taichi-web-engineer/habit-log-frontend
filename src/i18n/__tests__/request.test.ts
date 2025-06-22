d607e2e1ec627426d04941e0d2425ee4126ea664
taichi-web-engineer <aogaku415@gmail.com>
2025-06-22 17:05:21 +0900

    テスト失敗の試し

--- src/i18n/__tests__/request.test.ts
+++ src/i18n/__tests__/request.test.ts
@@ -14,7 +14,7 @@ const testCases = [
 		`cookie:${LOCALES.EN}、accept-language:ja,en;q=0.9`,
 		LOCALES.EN,
 		"ja,en;q=0.9",
-		LOCALES.EN,
+		LOCALES.JA,
 	],
 	[
 		`cookie:${LOCALES.JA}、accept-language:en,ja;q=0.9`,
