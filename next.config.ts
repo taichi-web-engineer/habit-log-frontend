0abe2643aa6352c4a092d64037a937ce53590891
taichi-web-engineer <aogaku415@gmail.com>
2025-08-16 09:40:42 +0900

    パッケージアップデート

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-	experimental: {
-		reactCompiler: true,
-	},
-};
+const nextConfig: NextConfig = {};
 export default createNextIntlPlugin("./src/features/i18n/request.ts")(
 	nextConfig,
 );
